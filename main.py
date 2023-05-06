9ab6db62118b12f237b0c2665c61ae07d7959fc9
Nicholas Grimaldi <nicholasgrimaldi42@gmail.com>
2023-05-06 01:08:53 -0400

    fixed backslash

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import os
 import sys
 
 # change dir
-path = getattr(sys, '_MEIPASS', os.getcwd())   
+path = getattr(sys, "_MEIPASS", os.getcwd())
 os.chdir(path)
 
 # Load the model
-model = load_model('assets\model.h5')
-icon = 'assets\code.ico'
+model = load_model("assets/model.h5")
+icon = "assets/code.ico"
 
 # set up the window
 root = tk.Tk()
@@ -28,27 +28,39 @@ root.configure(background=root.style.lookup("TFrame", "background"))
 # create the canvas
 canvas_width = 600
 canvas_height = 600
-canvas = tk.Canvas(root, width=canvas_width, height=canvas_height, bg='white')
+canvas = tk.Canvas(root, width=canvas_width, height=canvas_height, bg="white")
 canvas.pack()
 
 # set up the drawing functionality
 last_x, last_y = None, None
 line_width = 15
-line_color = 'black'
+line_color = "black"
+
 
 def draw(event):
     global last_x, last_y
     if last_x and last_y:
-        canvas.create_line(last_x, last_y, event.x, event.y, width=line_width, fill=line_color, capstyle='round')
+        canvas.create_line(
+            last_x,
+            last_y,
+            event.x,
+            event.y,
+            width=line_width,
+            fill=line_color,
+            capstyle="round",
+        )
     last_x, last_y = event.x, event.y
+
 
 def reset(event):
     global last_x, last_y
     last_x, last_y = None, None
 
+
 def clear_canvas():
     output_digit.set(" ")
     canvas.delete("all")
+
 
 def calculate_canvas_pixel_value(canvas):
     # Get the bounding box of the canvas
@@ -61,9 +73,12 @@ def calculate_canvas_pixel_value(canvas):
     img = ImageGrab.grab(bbox=(x, y, x1, y1))
 
     # Calculate the total pixel value of the image
-    pixel_value = np.sum(np.array(img.convert('L'))) / (canvas.winfo_width() * canvas.winfo_height())
+    pixel_value = np.sum(np.array(img.convert("L"))) / (
+        canvas.winfo_width() * canvas.winfo_height()
+    )
 
     return pixel_value
+
 
 def predict():
     # Calculate the pixel value of the canvas
@@ -73,54 +88,65 @@ def predict():
     if pixel_value > 252:
         output_digit.set("<N/A>")
         return
-    
+
     # Get the image from the canvas
     x = root.winfo_rootx() + canvas.winfo_x()
     y = root.winfo_rooty() + canvas.winfo_y()
     x1 = x + canvas.winfo_width()
     y1 = y + canvas.winfo_height()
-    img = ImageGrab.grab((x, y, x1, y1)).convert('L')
-    
+    img = ImageGrab.grab((x, y, x1, y1)).convert("L")
+
     # Resize the image to 28x28 pixels
     img = img.resize((28, 28))
-    
+
     # Normalize the pixel values
     img = ImageOps.invert(img)
     img = np.array(img) / 255.0
-    
+
     # Reshape the image into a 4D array (batch size, height, width, channels)
     img = img.reshape((1, 28, 28, 1))
-    
+
     # Make a prediction using the model
-    prediction = model.predict(img, verbose = 0)
+    prediction = model.predict(img, verbose=0)
 
     digit = np.argmax(prediction)
 
     output_digit.set(str(digit))
 
+
 # create the clear button
-clear_button = ttk.Button(root, text="Clear Canvas", command=clear_canvas, takefocus=False)
-clear_button.pack(side='left', padx=10, pady=10)
+clear_button = ttk.Button(
+    root, text="Clear Canvas", command=clear_canvas, takefocus=False
+)
+clear_button.pack(side="left", padx=10, pady=10)
 
 # Add the Predict button
 predict_button = ttk.Button(root, text="Predict", command=predict, takefocus=False)
-predict_button.pack(side='left', padx=10, pady=10)
+predict_button.pack(side="left", padx=10, pady=10)
 
 # setup output text
 output = tk.StringVar()
 output.set("You drew a: ")
 
-output_label = tk.Label(root, textvariable=output, font=('', 15), bg='#d9d9d9')
-output_label.pack(side='left', padx=10, pady=10)
+output_label = tk.Label(root, textvariable=output, font=("", 15), bg="#d9d9d9")
+output_label.pack(side="left", padx=10, pady=10)
 
 output_digit = tk.StringVar()
 output_digit.set(" ")
 
-output_digit_label = tk.Label(root, textvariable=output_digit, font=('', 15), bg='white', fg='black', width=5, height=1)
-output_digit_label.pack(side='left', padx=10, pady=10)
+output_digit_label = tk.Label(
+    root,
+    textvariable=output_digit,
+    font=("", 15),
+    bg="white",
+    fg="black",
+    width=5,
+    height=1,
+)
+output_digit_label.pack(side="left", padx=10, pady=10)
 
 # Bind the mouse events to the canvas
-canvas.bind('<B1-Motion>', draw)
-canvas.bind('<ButtonRelease-1>', reset)
+canvas.bind("<B1-Motion>", draw)
+canvas.bind("<ButtonRelease-1>", reset)
 
 root.mainloop()
